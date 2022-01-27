685f34f4b89884fb360c69b1bdb33ab285afc9bf
gaeul <e0540@hanmail.net>
2022-01-27 13:51:53 +0900

    충돌방지

--- food/settings.py
+++ food/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-030mmb337dftq&)78%do#fj_9l=n*2#mwxl@+%!q3)dbaf0-kh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =
 
 ALLOWED_HOSTS = []
 
