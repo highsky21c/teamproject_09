1d7fc5c43c2416434fb7e3867c07c519163d539d
김재성 <zhqmfkv@naver.com>
2022-01-28 20:25:39 +0900

    static 폴더 수정

--- food/settings.py
+++ food/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-030mmb337dftq&)78%do#fj_9l=n*2#mwxl@+%!q3)dbaf0-kh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
