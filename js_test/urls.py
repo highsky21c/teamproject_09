743a32374d664ee7abb5ae81e6dacf024478b529
김재성 <zhqmfkv@naver.com>
2022-02-03 11:54:50 +0900

    메인페이지/ 상세페이지 CSS 작성

--- js_test/urls.py
+++ js_test/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from js_test import views
 
 urlpatterns = [
-    path('', views.main)
+    path('', views.main, name='home'),
+    path('detail', views.detail, name='detail')
 ]
--- js_test/views.py
+++ js_test/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
-def main(request): #메인 화면
+def main(request):  # 메인 화면
     return render(request, 'home.html', {})
+
+
+def detail(request):
+    return render(request, 'detail.html', {})
