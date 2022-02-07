3ef68f3f08db0bf75cd90c26522ce5edccbd97f7
gaeul <e0540@hanmail.net>
2022-02-07 11:18:04 +0900

    아이디찾기 구현중

--- static/js/find-id.js
+++ static/js/find-id.js
@@ -1,3 +1,28 @@
+// csrf 토큰 받아오는 과정 - Ajax 사용하기 위함
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+///////////////////////
+
+
+
 $(document).ready(function() {
     //html의 body를 body라는 변수로 선언
     const body = document.querySelector("body");
@@ -22,4 +47,54 @@ function init(){
     paintImage(randomNumber);
 }
 init();
+
+var csrftoken = getCookie('csrftoken');
+$.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
 });
+
+
+
+$('#find-id').click(function () {
+    var name = $("#username").val();
+    var email = $("#email").val();
+
+    $.ajax({
+        type: "POST",
+        url: "/find-id/find/",
+        dataType: "json",
+        data: {
+            'name': name,
+            'email': email,
+        },
+        success: function (response) {
+            console.log(response)
+            $('#result_id').replaceWith(
+                '<div id="result_id"><hr><div style="text-align:center;"><span style="font-size: 16px;">입력된 정보로 가입된 아이디는 </span><span style="font-size: 20px; font-weight: bold;" id="result_id">' + response.result_id +
+                '</span><span style="font-size: 16px;"> 입니다.</span></div><hr></div>')
+        },
+        error: function () {
+            if (name == "" || email == "") {
+                alert('이름와 이메일을 입력해주세요.');
+            } else {
+                alert('입력하신 정보가 일치하지 않거나 존재하지 않습니다.');
+            }
+        },
+    });
+});
+
+});
+
+// function show_modal(){
+//     const modal = document.getElementById("modal");//html에서 id가 modal인애를 modal로 선언
+//     const find_id_btn = document.getElementById("find-id");//html에서 id가 find-id인애를 btn로 선언
+//     find_id_btn.addEventListener("click", e => {//버튼클릭했을때 모달을 띄우게.......?해도되는거니....
+//         modal.style.display = "flex"; //get으로 다른url로 나타내야하느것 아니뉘..?
+//     });
+// }
+
+
