0c509c637472146d41174226e3a3f70bc1c4e73f
김재성 <zhqmfkv@naver.com>
2022-02-07 18:41:22 +0900

    충돌해결 2

--- user/models.py
+++ user/models.py
@@ -3,10 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# Create your models here.
-class UserModel(AbstractUser):
-
-#model.py 작성 후 -> makemigrations, migrate
+# model.py 작성 후 -> makemigrations, migrate
 class UserModel(AbstractUser): #AbstractUser기능을 UserModel에 사용
 
     class Meta:
