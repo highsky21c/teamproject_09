0cd757c92d7872eccb85d9361e2ffada96ce1393
김재성 <zhqmfkv@naver.com>
2022-02-07 20:28:58 +0900

    중간 과정!

--- static/js/detail.js
+++ static/js/detail.js
@@ -43,10 +43,10 @@ $(document).ready(function () {
 
     var comments = document.getElementsByClassName('comment_row')
 
-    for(let i=0; i<comments.length; i++){
+    for (let i = 0; i < comments.length; i++) {
         const comment = comments[0]
-        if(i>=3){
-            comments[i].style.display='none'
+        if (i >= 3) {
+            comments[i].style.display = 'none'
         }
     }
 });
@@ -126,56 +126,55 @@ function favorite() {
         })
     }
 }
+//
+// // 좋아요 추가 및 삭제 함수
+// function like() {
+//     var like_off = document.getElementById('like_off')
+//     var like_on = document.getElementById('like_on')
+//     let storename = $('#store_name').text()
+//     if (like_off.style.display === 'flex') {
+//         //favorite off 상태
+//         like_off.style.display = 'none'
+//         like_on.style.display = 'flex'
+//         // $.ajax({
+//         //     type: 'POST',
+//         //     url: '/favorite/add',
+//         //     data: {'store_name': storename},
+//         //     success: function (response) {
+//         //         console.log(response['result'])
+//         //     }
+//         // })
+//     } else {
+//         like_off.style.display = 'flex'
+//         like_on.style.display = 'none'
+//         // $.ajax({
+//         //     type: 'POST',
+//         //     url: '/favorite/delete',
+//         //     data: {'store_name': storename},
+//         //     success: function (response) {
+//         //         console.log(response['result'])
+//         //     }
+//         // })
+//     }
+// }
 
-// 좋아요 추가 및 삭제 함수
-function like() {
-    var like_off = document.getElementById('like_off')
-    var like_on = document.getElementById('like_on')
-    let storename = $('#store_name').text()
-    if (like_off.style.display === 'flex') {
-        //favorite off 상태
-        like_off.style.display = 'none'
-        like_on.style.display = 'flex'
-        // $.ajax({
-        //     type: 'POST',
-        //     url: '/favorite/add',
-        //     data: {'store_name': storename},
-        //     success: function (response) {
-        //         console.log(response['result'])
-        //     }
-        // })
-    } else {
-        like_off.style.display = 'flex'
-        like_on.style.display = 'none'
-        // $.ajax({
-        //     type: 'POST',
-        //     url: '/favorite/delete',
-        //     data: {'store_name': storename},
-        //     success: function (response) {
-        //         console.log(response['result'])
-        //     }
-        // })
-    }
-}
-
-function comment_like(id) {
-    let check = document.getElementById(id)
-    let comment_id = id.split('_').pop()
-
-    var comment_like_on = document.getElementById('comment_'+ comment_id +'_on')
-    var comment_like_off = document.getElementById('comment_'+ comment_id +'_off')
-
-    if(comment_like_off.style.display ==='flex'){
-        comment_like_off.style.display = 'none'
-        comment_like_on.style.display = 'flex'
-    }
-    else{
-        comment_like_off.style.display = 'flex'
-        comment_like_on.style.display = 'none'
-    }
-    console.log(comment_like_on)
-
-}
+// function comment_like(id) {
+//     let check = document.getElementById(id)
+//     let comment_id = id.split('_').pop()
+//
+//     var comment_like_on = document.getElementById('comment_' + comment_id + '_on')
+//     var comment_like_off = document.getElementById('comment_' + comment_id + '_off')
+//
+//     if (comment_like_off.style.display === 'flex') {
+//         comment_like_off.style.display = 'none'
+//         comment_like_on.style.display = 'flex'
+//     } else {
+//         comment_like_off.style.display = 'flex'
+//         comment_like_on.style.display = 'none'
+//     }
+//     console.log(comment_like_on)
+//
+// }
 
 //슬라이더 함수
 var swiper = new Swiper(".content_photo_container", {
@@ -196,55 +195,90 @@ var swiper = new Swiper(".recommend_another", {
     },
 });
 
-function more(id){
+function more(id) {
     let comments = document.getElementsByClassName('comment_row')
     let comment_cnt = 0
     let more_button = document.getElementById(id)
-    for(let i =0; i<=comments.length; i++){
-        if(comments[i].style.display==='none'){
-            comment_cnt=i;
+    for (let i = 0; i <= comments.length; i++) {
+        if (comments[i].style.display === 'none') {
+            comment_cnt = i;
             break;
         }
     }
-    for(let k=comment_cnt; k<comment_cnt+3;k++){
-        if(k>=comments.length){
-            more_button.style.display='none'
+    for (let k = comment_cnt; k < comment_cnt + 3; k++) {
+        if (k >= comments.length) {
+            more_button.style.display = 'none'
             return
         }
-        comments[k].style.display ='flex'
+        comments[k].style.display = 'flex'
     }
 }
 
-function write_comments(){
+function write_comments() {
     const textarea = document.getElementById('comment_text')
     let text = textarea.value
     let scroll_value = $(document).scrollTop();
     $.ajax({
-        type:'post',
+        type: 'post',
         url: '/comment/write',
         data: {'comment': text},
-        success: function(response){
+        success: function (response) {
             console.log(response['result'])
         }
     })
 
     let comments = document.getElementsByClassName('comment_row')
-    for(let i=0; i<comments.length;i++){
-        comments[i].style.display='flex'
+    for (let i = 0; i < comments.length; i++) {
+        comments[i].style.display = 'flex'
     }
-    document.getElementById('more_button').style.display ='none'
+    document.getElementById('more_button').style.display = 'none'
     let a = $('.comment_row').clone()[0]
 
-    a.children[0].children[0].src ='/'
-    a.children[1].children[0].children[0].innerText ='username  '+comments.length
+    a.children[0].children[0].src = '/'
+    a.children[1].children[0].children[0].innerText = 'username  ' + comments.length
     a.children[1].children[2].children[0].innerText = text
-    a.children[2].id = 'comment_'+ comments.length
-    a.children[2].children[0].children[0].id = 'comment_'+comments.length +'_on'
-    a.children[2].children[0].children[1].id = 'comment_'+comments.length +'_off'
+    a.children[2].id = 'comment_' + comments.length
+    a.children[2].children[0].children[0].id = 'comment_' + comments.length + '_on'
+    a.children[2].children[0].children[1].id = 'comment_' + comments.length + '_off'
     console.log(a.children[2].children[0].children[1].id)
 
-    comments[comments.length-1].after(a)
+    comments[comments.length - 1].after(a)
 
     window.scrollTo(0, scroll_value)
     textarea.value = ''
 }
+
+
+$(".cmt_like").click(function () { // .like 버튼을 클릭 감지
+    var pk = $(this).attr('name')
+    $.ajax({ // ajax로 서버와 통신
+        type: "POST", // 데이터를 전송하는 방법
+        url: "{% url 'contents:cmt_like' %}", // 통신할 url을 지정
+        data: {'pk': pk, 'csrfmiddlewaretoken': '{{ csrf_token }}'}, // 서버로 데이터 전송시 옵션, pk를 넘겨야 어떤거지 알 수 있음
+        dataType: "json",
+        success: function (response) { // 성공
+            $("#count-cmt-like-" + pk).html("좋아요&nbsp;" + response.cmt_likes_count + "개"); // 좋아요 개수 변경
+        },
+        error: function (request, status, error) { // 실패
+            alert("로그인이 필요합니다.")
+            window.location.replace("/sign-in/") // 로그인 페이지로 넘어가기
+        },
+    });
+})
+
+$(".cmt_hate").click(function () { // .hate 버튼을 클릭 감지
+    var pk = $(this).attr('name')
+    $.ajax({ // ajax로 서버와 통신
+        type: "POST", // 데이터를 전송하는 방법
+        url: "{% url 'contents:cmt_hate' %}", // 통신할 url을 지정
+        data: {'pk': pk, 'csrfmiddlewaretoken': '{{ csrf_token }}'}, // 서버로 데이터 전송시 옵션, pk를 넘겨야 어떤거지 알 수 있음
+        dataType: "json",
+        success: function (response) { // 성공
+            $("#count-cmt-hate-" + pk).html("싫어요&nbsp;" + response.cmt_hates_count + "개");
+        },
+        error: function (request, status, error) { // 실패
+            alert("로그인이 필요합니다.")
+            window.location.replace("/sign-in/") // 로그인 페이지로 넘어가기
+        },
+    });
+})
