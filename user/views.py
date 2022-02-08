310c582ff88ca1116687ef2f4b76655ef2bcc12e
김재성 <zhqmfkv@naver.com>
2022-02-08 19:33:02 +0900

    picture 수정!

--- user/views.py
+++ user/views.py
@@ -91,7 +91,9 @@ def load_my_profile(request):
     user = UserModel.objects.get(id=user_id)
     my_favorite = Favorite.objects.filter(user=user).order_by('date')
     menu_list = []
+    picture_list = []
     for favorite in my_favorite:
         menu_list.append(json.loads(favorite.store.menu.replace('\'', '\"')))
+        picture_list.append(json.loads(favorite.store.pic.replace('\'', '\"'))[0])
     store_list = Store.objects.order_by('?')[0:5]
-    return render(request, 'profile.html', {'favorite_store': my_favorite, 'menu_list': menu_list, 'recommend': store_list})
+    return render(request, 'profile.html', {'favorite_store': my_favorite, 'menu_list': menu_list, 'recommend': store_list, 'store_pic': picture_list})
